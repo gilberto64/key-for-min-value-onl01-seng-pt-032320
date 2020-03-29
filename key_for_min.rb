8b3c76cfcad2f607d5d0e025357b6e5f3c39575d
Gilberto Ocampo <ocampo@gate.net>
2020-03-29 00:36:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   lowest_counter = 99999999
   
   name_hash.each do |key,value| 
-    if value[counter2] < value[counter2 + 1] then lowest_counter = counter 2 
+    if value[counter2] < value[counter2 + 1] then lowest_counter = counter2 
     end
   end 
   puts "the lowest key is #{key}"      
