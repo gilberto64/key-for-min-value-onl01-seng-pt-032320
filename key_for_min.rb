a40ff996ec9d02d01988b5f56e00c4a6c6a38840
Gilberto Ocampo <ocampo@gate.net>
2020-03-29 00:34:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,7 @@ def key_for_min_value(name_hash)
   counter2 = 0
   lowest_counter = 99999999
   
-  name_hash.each 
-  
+  name_hash.each do |key,value| 
     if value[counter2] < value[counter2 + 1] then lowest_counter = counter 2 
     end
   end 
