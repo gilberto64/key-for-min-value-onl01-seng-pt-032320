c5386ce1c467d96b9f3e4e4c9366541bced3af19
Gilberto Ocampo <ocampo@gate.net>
2020-03-29 00:38:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,9 @@ def key_for_min_value(name_hash)
   counter2 = 0
   lowest_counter = 99999999
   
-  name_hash.each do |stkey,value| 
-    if value[counter2] < value[counter2 + 1] then lowest_counter = counter2 
+  name_hash.each do |str,num| 
+    if num[counter2] < num[counter2 + 1] then lowest_counter = counter2 
     end
   end 
-  puts "the lowest key is #{key}"      
+  puts "the lowest key is #{str}"      
 end
