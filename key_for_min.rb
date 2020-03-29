6faadd37a6d7416bc93f2e13744c2c067b0a0a57
Gilberto Ocampo <ocampo@gate.net>
2020-03-29 00:26:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
+  counter1 = name_hash.length
+  counter2 = 0
+  lowest_counter = 99999999
+  
+  while counter2 < counter1
+    if value[counter2] < value[counter2 + 1] lowest_counter = counter 2 
+    end
+  end 
+  puts the lowest     
+      .each do |key, value|
     if value < 999999 then puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
  end 
